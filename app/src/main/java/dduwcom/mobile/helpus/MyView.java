161a0b8277030db7678d8ad3aab89218b30d8d1d
leemj <20190988@dongduk.ac.kr>
2021-07-23 18:43:41 +0900

    mj commit2

--- app/src/main/java/dduwcom/mobile/helpus/MyView.java
+++ app/src/main/java/dduwcom/mobile/helpus/MyView.java
@@ -1,4 +1,32 @@
 package dduwcom.mobile.helpus;
 
-public class MyView {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MyView extends View {
+    Paint pnt = new Paint();
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(R.color.main);
+        super.onDraw(canvas);
+    }
 }
